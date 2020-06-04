c09682feb3423e4ffea2824f0120efa75cac2c2a
Nash Luffman <nashluffman@gmail.com>
2020-06-03 21:30:16 -0400

    working api

---
+++ scraper.py
@@ -0,0 +1,70 @@
+import requests, json
+from bs4 import BeautifulSoup
+'''
+Example JSON response of trail status
+{
+  "data":[
+    {
+      "trail":"Beaver Dam",
+      "status":"open",
+      "last_updated":"05/30 9:03am"
+    },
+    {
+      "trail":"Briar Chapel",
+      "status":"closed",
+      "last_updated":"05/31 10:34am"
+    },
+    {
+      "trail":"Brumley Forest",
+      "status":"closed",
+      "last_updated":"05/29 4:52pm"
+    }
+  ]
+}
+'''
+
+
+status = requests.get('https://www.trianglemtb.com/mobiletrailstatus.php')
+
+soup = BeautifulSoup(status.content, 'html.parser')
+
+trails = soup.find_all('tr')
+
+class Trail:
+
+    def __init__(self, trail):
+        self.trail = trail
+        self.name = None
+        self.status = None
+    
+    def getNameAndDate(self):
+        nameAndDate = self.trail.find_all('td', class_ = 'trail')
+        for value in nameAndDate: 
+            trailName = value.find('a').text if value.find('a') else None 
+            if trailName:
+                self.name = trailName.encode('ascii', errors='ignore').decode().strip()
+            else:
+                self.date = value.text
+
+    def getStatus(self):
+        trailOpen = self.trail.find('td', class_ = 'open')
+        trailPartiallyOpen = self.trail.find('td', class_='info')
+        if trailOpen or trailPartiallyOpen:
+            self.status = 'open'
+        else:
+            self.status = 'closed'
+allTrailData = []
+for trail in trails:
+    trailData = {}
+    currentTrail = Trail(trail)
+    currentTrail.getNameAndDate()
+    currentTrail.getStatus()
+    if currentTrail.name:
+        trailData['trail'] = currentTrail.name
+        trailData['status'] = currentTrail.status
+        trailData['last_updated'] = currentTrail.date
+        allTrailData.append(trailData)
+    else:
+        next
+result = {'data':allTrailData}
+print(json.dumps(result, indent=2))
